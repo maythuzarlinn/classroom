778c9864ab6a5d1b52f61a00d4b0c1a9062cb62f
maythuzarlinn <maythuzarlinn96@gmail.com>
2025-08-29 10:56:19 +0630

    Added nav icon update.

--- resources/views/includes/commons/menu.blade.php
+++ resources/views/includes/commons/menu.blade.php
@@ -8,6 +8,41 @@
         <span class="d-none d-lg-inline">Dashboard</span>
         <span class="d-none d-lg-inline badge bg-primary rounded-pill float-end">8</span>
     </a>
+    <a href="{{ route('schoolclasses.index') }}" class="list-group-item list-group-item-action">
+        <i class="fa-solid fa-calendar"></i>
+        <span class="d-none d-lg-inline">Classes Schedule</span>
+    </a>
+    <a href="{{ route('attendances.index') }}" class="list-group-item list-group-item-action">
+        <i class="fa-regular fa-pen-to-square"></i>
+        <span class="d-none d-lg-inline">Attendance</span>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+        <i class="fa-solid fa-folder-open"></i>
+        <span class="d-none d-lg-inline">Assignments</span>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+        <i class="fa-regular fa-calendar-days"></i>
+        <span class="d-none d-lg-inline">Exam / Test</span>
+    </a>    
+</div>
+<!-- Admin Panel -->
+<div class="list-group mt-4 text-center text-lg-start">
+    <span class="list-group-item disabled d-none d-lg-block">
+        <i class="fa-solid fa-gear"></i>
+        <h5 class="d-none d-lg-inline">Setting</h5>
+    </span>
+    <a href="{{ route('classrooms.index') }}" class="list-group-item list-group-item-action">
+        <i class="fa-solid fa-landmark"></i>
+        <span class="d-none d-lg-inline">Classrooms</span>
+    </a>
+    <a href="{{ route('grades.index') }}" class="list-group-item list-group-item-action">
+        <i class="fa-solid fa-user-graduate"></i>
+        <span class="d-none d-lg-inline">Grade</span>
+    </a>
+    <a href="{{ route('subjects.index') }}" class="list-group-item list-group-item-action">
+        <i class="fa-solid fa-book-bookmark"></i>
+        <span class="d-none d-lg-inline">Subjects</span>
+    </a>
     <a href="{{ route('students.index') }}" class="list-group-item list-group-item-action">
         <i class="fa-solid fa-users"></i>
         <span class="d-none d-lg-inline">Students</span>
@@ -15,31 +50,5 @@
     <a href="{{ route('teachers.index') }}" class="list-group-item list-group-item-action">
         <i class="fa-solid fa-user"></i>
         <span class="d-none d-lg-inline">Teachers</span>
-    </a>
-    <a href="{{ route('attendances.index') }}" class="list-group-item list-group-item-action">
-        <i class="fa-regular fa-pen-to-square"></i>
-        <span class="d-none d-lg-inline">Attendance</span>
-    </a>    
-    <a href="{{ route('schoolclasses.index') }}" class="list-group-item list-group-item-action">
-        <i class="fa-solid fa-flag"></i>
-        <span class="d-none d-lg-inline">Classes</span>
-    </a>
-</div>
-<!-- Admin Panel -->
-<div class="list-group mt-4 text-center text-lg-start">
-    <span class="list-group-item disabled d-none d-lg-block">
-        <h5>School admin panel</h5>
-    </span>
-    <a href="{{ route('classrooms.index') }}" class="list-group-item list-group-item-action">
-        <i class="fa-regular fa-building"></i>
-        <span class="d-none d-lg-inline">Classrooms</span>
-    </a>    
-    <a href="{{ route('grades.index') }}" class="list-group-item list-group-item-action">
-        <i class="fa-regular fa-calendar-minus"></i>
-        <span class="d-none d-lg-inline">Grade</span>
-    </a>
-    <a href="{{ route('subjects.index') }}" class="list-group-item list-group-item-action">
-        <i class="fa-regular fa-calendar-days"></i>
-        <span class="d-none d-lg-inline">Subjects</span>
     </a>
 </div>
